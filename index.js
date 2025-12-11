22bbfeb778228fc4406f830ca1bc24d08bece937
vazgucxn <vazgu3z@gmail.com>
2025-12-11 22:41:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -762,9 +762,9 @@ if (cmd === "patlat") {
 
     setTimeout(async () => {
         const embed5 = new EmbedBuilder()
-            .setColor("#000000")
-            .setTitle("💥 PATLAMA GERÇEKLEŞTİ 💥")
-            .setDescription(`**Allah başarıyla patlatıldı!**\n\n> *Şaka yaptım yarram 🤣*`);
+    .setColor("#000000")
+    .setTitle("💥 PATLAMA GERÇEKLEŞTİ 💥")
+    .setDescription(`**Allah başarıyla patlatıldı!**\n\n> *Şaka yaptım yarram 🤣*`);
 
         await msg.edit({ embeds: [embed5] });
     }, 4000);
@@ -1140,6 +1140,7 @@ client.on("userUpdate", async (oldUser, newUser) => {
 //                         BOT LOGIN
 // ===================================================================
 client.login(TOKEN);
+
 
 
 
